709c973f26b2fa6250488fa11b3990bb4c411f80
Guillaume ROBIN <robinguillaume.pro@gmail.com>
2018-01-11 00:55:54 +0100

    Cleaning repository

--- src/IGate.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * Created by cesumilo
- * Author: Guillaume ROBIN <robinguillaume.pro@gmail.com>
- * Date: 10/01/2018
- * Licence: All rights reserved @ Guillaume ROBIN <robinguillaume.pro@gmail.com>
- */
-Object.defineProperty(exports, "__esModule", { value: true });
